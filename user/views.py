13ee742b90882bbf44bd524a4a8a866bd9236546
Taras Ivashchuk <tvishy@disroot.org>
2026-01-05 20:30:11 +0200

    clean up

--- user/views.py
+++ user/views.py
@@ -2,11 +2,8 @@ from rest_framework import generics, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.settings import api_settings
-from rest_framework.views import APIView
 
-from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 from user.serializers import UserSerializer
 
 
